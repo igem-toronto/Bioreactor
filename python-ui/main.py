a66665cc03ff8432a9008e3deecc10cf5a82494e
Lia-MC <lia.mc73700@gmail.com>
2026-07-26 10:02:55 -0400

    outlined basic functions

--- python-ui/main.py
+++ python-ui/main.py
@@ -1,4 +1,4 @@
-# CONSTANTS
+# constants
 
 PORTS = [
     "COM3",
@@ -12,3 +12,30 @@ PORTS = [
 ]
 
 BAUDRATE = 115200
+
+# bioreactor class
+class Bioreactor:
+
+# connect to serial ports
+def connect_serial():
+
+# read data from serial ports
+def serial_thread():
+
+# parse data from serial port
+def parse_data(reactor_number, line):
+
+# send command to bioreactor via serial port
+def send_command():
+
+# update graphs with new data w every refresh
+def update_graphs():
+
+# update tkinter / other gui w every refresh
+def update_gui():
+
+# save data to CSV file
+def save_csv():
+
+
+if __name__ == "__main__":
